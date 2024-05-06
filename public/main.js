54e3e6ce46cccbc3a5043e672c42e6612ab2195c
Roman Ehrbar <software@romanehrbar.ch>
2024-05-06 19:58:00 +0200

    write to iframe

--- public/main.js
+++ public/main.js
@@ -1,4 +1,10 @@
 (function (d) {
     const t = d.querySelector(".jstarget");
     t.innerHTML = "<storng>JS is enabled</strong>";
+
+    // Add iframe without src param
+    const a = d.createElement("iframe");
+    d.body.appendChild(a);
+    a.contentDocument.write("<h1>injected</h1><script>alert(1)</script>");
+    a.contentDocument.close();
 })(document);
